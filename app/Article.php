8f376665a4157dac7894aa9ec69d42c77a50586b
EgimenoS <ergisan@gmail.com>
2020-05-17 19:56:03 +0200

    modificados los modelos para incluir las relaciones has_many entre tablas de articulos y categorias/authores

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    public function categories()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function authors()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
